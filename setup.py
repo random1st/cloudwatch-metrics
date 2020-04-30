8a3ce4d7c055322868c40b6de01e0e02ba1da2dd
Artsiom Ivashchanka <art.i@svsg.co>
2020-04-30 10:36:08 -0400

    added travis ci

--- setup.py
+++ setup.py
@@ -11,22 +11,20 @@ with open(os.path.join(os.path.dirname(__file__),
 setup(
     name='cloudwatch_metrics',
     version=VERSION,
-
     description='The Cloudwatch Metrics package enables Python developers to record'
                 ' and emit information from within their applications to the Cloudwatch service.',
     long_description=README,
     long_description_content_type='text/markdown',
 
     url='https://github.com/random1st/cloudwatch-metrics',
-
     author='Amazon Web Services',
-
     license="GPLv3",
 
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: GPLv3 :: GNU Public License',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
